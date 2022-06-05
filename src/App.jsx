81ef68ccfff724849ab2efcbe58180304ee6f4d3
mdestafadilah <desta.08b@gmail.com>
2022-06-05 14:51:56 +0700

    #2 BASIC QUERY

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,22 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
+import { useQuery  } from 'react-query'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const queryInfo = useQuery('pokemon',() => 
+    axios
+      .get('https://pokeapi.co/api/v2/pokemon')
+      .then(res => res.data.results)
+  )
+  console.log(queryInfo);
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      {queryInfo.data?.map(result => {
+        return <div key={result.name}>
+          {result.name}
+        </div>
+      })}
     </div>
   )
 }
