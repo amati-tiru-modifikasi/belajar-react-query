7dbaf3e15dbeb41ecb1cef6fa2a94d808a135de7
mdestafadilah <desta.08b@gmail.com>
2022-06-05 16:38:06 +0700

    #12 Paralel Queries

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
     <div>
       <Count />
       <Pokemon />
+      <Berries />
     </div>
   )
 }
@@ -24,6 +25,27 @@ function usePokemon() {
 function Count() {
   const queryInfo = usePokemon()
   return <h3>Your ar looking at {queryInfo.data?.length} pokemon</h3>
+}
+
+function Berries() {
+  const queryInfo = useQuery("berries", async () => {
+    await new Promise(resolve => setTimeout(resolve, 1000))
+    return axios
+      .get("https://pokeapi.co/api/v2/berry")
+      .then((res) => res.data.results)
+    });
+
+  return queryInfo.isLoading ? (
+    "Loading..."
+  ) : queryInfo.isError ? (
+    queryInfo.error.message
+  ) : (
+    <div>
+      {queryInfo.data?.map((result) => {
+        return <div key={result.name}>{result.name}</div>;
+      })}
+    </div>
+  );
 }
 
 function Pokemon() {
